dd19092098f252733392b04f7cfb9178ab90f239
mistry-jay <jaymistry3103@gmail.com>
2021-01-21 15:56:57 +0530

    initital commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+//fix Header for user-profle
+function sticky_header() {
+    var header_height = jQuery('.site-header').innerHeight() / 2;
+    var scrollTop = jQuery(window).scrollTop();;
+    if (scrollTop > header_height) {
+        jQuery('body').addClass('sticky-header');
+    } else {
+        jQuery('body').removeClass('sticky-header');
+    }
+}
+
+jQuery(document).ready(function () {
+  sticky_header();
+});
+
+jQuery(window).scroll(function () {
+  sticky_header();  
+});
+jQuery(window).resize(function () {
+  sticky_header();
+});
